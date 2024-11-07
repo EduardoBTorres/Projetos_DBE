6dab892e315b2c72159a5b31c62d9ad292922fd3
eduardotorres <eduardotorrespl012@academico.ifsul.edu.br>
2024-11-07 18:26:20 -0300

    Crud Completo

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Rotas de READ
 Route::get('/bicicletas', [BicicletaController::class,'index']);
 
 Route::get('/bicicleta/{id}', [BicicletaController::class, 'show'])->name('show');
 
+//Rotas de CREATE
 Route::get('/bicicleta', [BicicletaController::class, 'create']);
 
 Route::post('/bicicleta', [BicicletaController::class, 'store']);
 
+//Rotas de UPDATE
 Route::get('/bicicleta/{id}/edit', [BicicletaController::class, 'edit'])->name('edit');
 
 Route::post('/bicicleta/{id}/update', [BicicletaController::class, 'update'])->name('update');
 
+//Rota de DELETE
 Route::get('/bicicleta/{id}/delete', [BicicletaController::class, 'delete'])->name('delete');
